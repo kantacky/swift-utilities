411f0cdefce00350d04b9ef03e518de5e049ea0b
Kanta Oikawa <k.oikawa@kantacky.com>
2024-05-28 11:56:59 +0900

    Add documentations

--- Sources/SwiftUtilities/Version/Version.swift
+++ Sources/SwiftUtilities/Version/Version.swift
@@ -8,11 +8,49 @@
 import Foundation
 import RegexBuilder
 
+/// A version according to the semantic versioning specification.
+///
+/// A package version consists of three integers separated by periods, for example `1.0.0`. It must conform to the semantic versioning standard in order to ensure
+/// that your package behaves in a predictable manner once developers update their
+/// package dependency to a newer version. To achieve predictability, the semantic versioning specification proposes a set of rules and
+/// requirements that dictate how version numbers are assigned and incremented. To learn more about the semantic versioning specification, visit
+/// [Semantic Versioning 2.0.0](https://semver.org).
+///
+/// - term The major version: The first digit of a version, or _major version_,
+/// signifies breaking changes to the API that require updates to existing
+/// clients. For example, the semantic versioning specification considers
+/// renaming an existing type, removing a method, or changing a method's
+/// signature breaking changes. This also includes any backward-incompatible bug
+/// fixes or behavioral changes of the existing API.
+///
+/// - term The minor version:
+/// Update the second digit of a version, or _minor version_, if you add
+/// functionality in a backward-compatible manner. For example, the semantic
+/// versioning specification considers adding a new method or type without
+/// changing any other API to be backward-compatible.
+///
+/// - term The patch version:
+/// Increase the third digit of a version, or _patch version_, if you're making
+/// a backward-compatible bug fix. This allows clients to benefit from bugfixes
+/// to your package without incurring any maintenance burden.
 public struct Version {
+
+    /// The major version according to the semantic versioning standard.
     public let major: Int
+
+    /// The minor version according to the semantic versioning standard.
     public let minor: Int
+
+    /// The minor version according to the semantic versioning standard.
     public let patch: Int?
 
+    /// Initializes a version struct with the provided components of a semantic version.
+    /// - Parameters:
+    ///   - major: The major version number.
+    ///   - minor: The minor version number.
+    ///   - patch: The patch version number.
+    ///
+    /// - Precondition: `major >= 0 && minor >= 0 && patch >= 0`.
     public init(
         _ major: Int,
         _ minor: Int,
@@ -74,13 +112,22 @@ public struct Version {
     }
 }
 
-extension Version: Equatable {
+extension Version: Comparable {
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`, `a ==
+    /// b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    ///
+    /// - Returns: A boolean value indicating the result of the equality test.
     public static func == (lhs: Version, rhs: Version) -> Bool {
         lhs.major == rhs.major && lhs.minor == rhs.minor && lhs.safePatch == rhs.safePatch
     }
-}
 
-extension Version: Comparable {
     public static func < (lhs: Version, rhs: Version) -> Bool {
         if (lhs.major != rhs.major) {
             return lhs.major < rhs.major
